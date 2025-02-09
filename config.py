f1683e2f4c5bbb4444475b853de573a881eedb26
rosoporto <rosoporto@yandex.ru>
2025-02-09 23:50:58 +0300

    add config

---
+++ config.py
@@ -0,0 +1,8 @@
+type_of_development = "development"
+
+cities = {
+    "Москва": "Moscow",
+    "Санкт-Петербург": "Saint-Petersburg",
+    "Новосибирск": "Novosibirsk",
+    "Екатеринбург": "Yekaterinburg",
+}
